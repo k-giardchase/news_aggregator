bc5b132458647b7b4d293a2917f2d819d353a058
Kyle Giard-Chase & Brian Kropff <student@epicodus.com>
2015-04-22 14:07:14 -0700

    Add category and description to topic objects

--- services/TopicsFactory.js
+++ services/TopicsFactory.js
@@ -2,8 +2,10 @@ nerdSourced.factory('TopicsFactory', function TopicsFactory() {
   var factory = {};
   factory.topics = [];
   factory.addTopic = function() {
-    factory.topics.push({ title: factory.topicTitle, id: factory.topics.length + 1, comments: [] });
+    factory.topics.push({ title: factory.topicTitle, description: factory.topicDescription, category: factory.topicCategory, id: factory.topics.length + 1, comments: [] });
     factory.topicTitle = null;
+    factory.topicDescription = null;
+    factory.topicCategory = null;
   }
   return factory;
 });
